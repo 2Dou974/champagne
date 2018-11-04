286bf14bc8e1a449900b6dfa18b44178d62144eb
2Dou974 <olivier.ardouvin@gmail.com>
2018-11-04 11:24:21 +0400

    legal modal juste code

---
+++ wp-content/themes/champagne/js/common.js
@@ -0,0 +1,40 @@
+var chCommon = {};
+
+chCommon.readCookie = function(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+chCommon.getParameterValues = function(param) {
+    var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < url.length; i++) {
+        var urlparam = url[i].split('=');
+        if (urlparam[0] == param) {
+            return urlparam[1];
+        }
+    }
+    return null;
+}
+
+$(function() {
+    //check legal age
+    $('.modalLegalAge .btn').off('click').on('click', function() {
+        //add cookie oag a 1
+        document.cookie = "oag=1;; path=/";
+        $('.modalLegalAge').modal({
+            show: hide
+        });
+    });
+    if (chCommon.readCookie('oag') == null && chCommon.getParameterValues('oag') == null) {
+        $('.modalLegalAge').modal({
+            show: true,
+            keyboard: false,
+            backdrop: false
+        })
+    }
+});
---
+++ wp-content/themes/champagne/modal/legalAge.php
@@ -0,0 +1,14 @@
+<div class="modal  modalLegalAge" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      
+      <div class="modal-body">
+        <p>Pour visiter notre site, vous devez être en âge de consommer et/ou d'acheter de l'alcool selon la législation en vigueur dans votre localisation de résidence. S'il n'existe pas de législation à cet égard dans votre localisation vous devez être âgé d'au moins 21 ans.</p>
+     
+      
+        <button type="button" class="btn btn-primary">Oui, j'ai l'âge légal</button>
+        </p>En cliquant sur "Oui" j’accepte les <a href="/legalMention?oag=1" target='_blank'>Conditions Générales d’Utilisation</a> et déclare avoir lu la <a href="/cookie?oag=1" target='_blank'>Charte données personnelles & cookies</a>.</p>
+      </div>
+    </div>
+  </div>
+</div>
