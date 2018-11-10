ae83ab9f093893fac5a6e71bc04d345ddfd2fa5b
2Dou974 <olivier.ardouvin@gmail.com>
2018-11-10 22:50:46 +0400

    menu

--- wp-content/themes/champagne/modal/legalAge.php
+++ wp-content/themes/champagne/modal/legalAge.php
@@ -2,10 +2,17 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-body">
-                <p>Pour visiter notre site, vous devez être en âge de consommer et/ou d'acheter de l'alcool selon la législation en vigueur dans votre localisation de résidence. S'il n'existe pas de législation à cet égard dans votre localisation vous devez
-                    être âgé d'au moins 21 ans.</p>
-                <button type="button" class="btn btn-primary">Oui, j'ai l'âge légal</button>
-                </p>En cliquant sur "Oui" j’accepte les <a href="/legalMention?oag=1" target='_blank'>Conditions Générales d’Utilisation</a> et déclare avoir lu la <a href="/cookie?oag=1" target='_blank'>Charte données personnelles & cookies</a>.</p>
+                <!-- logo -->
+                <div class="roundedImage">
+                    <img src="<?php bloginfo('template_directory');?>/img/logo_150.png" title="<?php bloginfo('name');?>" />
+                </div>
+                <!-- text age -->
+                <p>Pour visiter notre site, vous devez être en âge de consommer et/ou d'acheter de l'alcool selon la législation en vigueur dans votre localisation de résidence.<br/>
+                 S'il n'existe pas de législation à cet égard dans votre localisation vous devez être âgé d'au moins 21 ans.</p>
+                <!-- btn ok -->
+                <button type="button" class="btn btn-silver">Oui, j'ai l'âge légal</button>
+                <!-- validation cookies -->
+                <p>En cliquant sur "Oui" j’accepte les <a href="/legalMention?oag=1" target='_blank'>Conditions Générales d’Utilisation</a> et déclare avoir lu la <a href="/cookie?oag=1" target='_blank'>Charte données personnelles & cookies</a>.</p>
             </div>
         </div>
     </div>
