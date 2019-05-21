81aac092cbf4f67f39154d04b4966db74e5ad67d
2Dou974 <olivier.ardouvin@gmail.com>
2019-05-22 02:31:54 +0400

    add  ga UA-8389156-7

--- wp-content/themes/champagne/footer-champagne.php
+++ wp-content/themes/champagne/footer-champagne.php
@@ -10,6 +10,16 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="<?php bloginfo('template_directory');?>/js/common.js"></script>
     <script src="<?php bloginfo('template_directory');?>/js/champagne.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-8389156-7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-8389156-7');
+</script>
+
 </body>
 
 </html>
--- wp-content/themes/champagne/footer.php
+++ wp-content/themes/champagne/footer.php
@@ -9,6 +9,16 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="<?php bloginfo('template_directory');?>/js/common.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-8389156-7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-8389156-7');
+</script>
+
 </body>
 
 </html>
